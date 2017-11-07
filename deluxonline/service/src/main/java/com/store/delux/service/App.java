e58981f07f3d37f9220176329792ef9b10579cb7
Eder <Eder@LAPTOP-1TH4EU38>
2017-11-06 22:28:14 -0600

    Head

---
+++ deluxonline/service/src/main/java/com/store/delux/service/App.java
@@ -0,0 +1,13 @@
+package com.store.delux.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
