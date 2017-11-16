f62c37be456c68baf5df816b3c151d9067588922
Eder <Eder@LAPTOP-1TH4EU38>
2017-11-15 23:25:34 -0600

    changes 11/15

---
+++ deluxonline/web/src/main/java/com/store/delux/web/bean/CountryBean.java
@@ -0,0 +1,57 @@
+package com.store.delux.web.bean;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import com.store.delux.service.dto.CountryDTO;
+import com.store.delux.service.service.CountryService;
+import com.store.delux.service.service.impl.CountryServiceImpl;
+
+@ManagedBean(name = "countryBean")
+@ViewScoped
+public class CountryBean implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private CountryDTO country;
+	private Integer countryId;
+	
+	private CountryService countryService;
+	
+	public CountryBean(){
+		country = new CountryDTO();
+		countryService = new CountryServiceImpl();
+		
+	}
+	
+	public void save() throws Exception{
+		countryService.save(country);
+	}
+	
+	public String getCountrys() throws Exception{
+		country = countryService.getVountry(countryId);
+		
+		return null;
+	}
+
+	public Integer getCountryId() {
+		return countryId;
+	}
+
+	public void setCountryId(Integer countryId) {
+		this.countryId = countryId;
+	}
+		
+	public CountryDTO getCountry() {
+		return country;
+	}
+
+	public void setCountry(CountryDTO country) {
+		this.country = country;
+	}
+	
+	
+
+}
