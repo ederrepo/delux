9e3e492c2d2a544813f950b0defd110e95e3af64
Eder <Eder@LAPTOP-1TH4EU38>
2017-11-22 21:22:03 -0600

    mapeos

--- deluxonline/service/src/main/java/com/store/delux/service/service/impl/CountryServiceImpl.java
+++ deluxonline/service/src/main/java/com/store/delux/service/service/impl/CountryServiceImpl.java
@@ -3,6 +3,7 @@ package com.store.delux.service.service.impl;
 import com.store.delux.db.dao.CountryDAO;
 import com.store.delux.db.dao.impl.CountryDAOImpl;
 import com.store.delux.db.entity.Country;
+import com.store.delux.db.utils.HibernateUtil;
 import com.store.delux.service.dto.CountryDTO;
 import com.store.delux.service.service.CountryService;
 
@@ -12,12 +13,26 @@ public class CountryServiceImpl implements CountryService {
 	
 	public Integer save(CountryDTO country) throws Exception {
 		Country countryDb = new Country();
+		Integer result = null;
+		System.out.println("SAVE 2");
+		try{
+			countryDb.setCountryId(country.getCountryId());
+			countryDb.setCountryNm(country.getCountryNm());
+			
+			result = countryDao.save(countryDb);
+			
+			System.out.println("SAVE 5: " + result);
+			
+		}catch(Exception e){
+			HibernateUtil.closeSession(false);
+			System.out.println("SAVE 6");
+		}finally{
+			HibernateUtil.closeSession(true);
+			System.out.println("SAVE 7");
+		}	
 		
-		countryDb.setCountryId(country.getCountryId());
-		countryDb.setCountryNm(country.getCountryNm());
-		
-		Integer result = countryDao.save(countryDb);
-		
+		System.out.println("SAVE 8");
+
 		return result;
 	}
 
