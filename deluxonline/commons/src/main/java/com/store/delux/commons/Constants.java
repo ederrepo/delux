a3add87d3797b90fddb76bf0b0a972826eb6572e
Eder <Eder@LAPTOP-1TH4EU38>
2018-01-27 20:06:12 -0600

    27-01

--- deluxonline/commons/src/main/java/com/store/delux/commons/Constants.java
+++ deluxonline/commons/src/main/java/com/store/delux/commons/Constants.java
@@ -8,5 +8,11 @@ public class Constants {
 	public static final Integer MESSAGE_TYPE_WARM = 1;
 	public static final Integer MESSAGE_TYPE_ERROR = 2;
 	public static final Integer MESSAGE_TYPE_FATAL = 3;
+	
+	public static final Integer CATEGORY_LAPTOP_TYPE = 1;
+	public static final Integer CATEGORY_DESKTOP_TYPE = 2;
+	public static final Integer CATEGORY_MONITOR_TYPE = 3;
+	public static final Integer CATEGORY_MULTIFUNTIONAL_TYPE = 4;
+	public static final Integer CATEGORY_PRINTER_TYPE = 5;
 
 }
